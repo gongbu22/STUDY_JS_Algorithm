fa3a9f5342d47065ea946fb7fb2932cc77415f02
gongbu22 <pyujin0711@naver.com>
2025-03-10 22:57:39 +0900

    재귀함수 예시시

---
+++ length_of_str.mjs
@@ -0,0 +1,9 @@
+//글자수세기
+function strLength(arr){
+    if(arr[0] == null) return 0;
+    return strLength(arr.slice(0, -1)) + 1;
+}
+
+let str = "abcde";
+let len = strLength(str);
+console.log(len);
